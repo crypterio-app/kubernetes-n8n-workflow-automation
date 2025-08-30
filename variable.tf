ea48815f8df95aa48bab0be2f6ea5b973952e429
Yevgeni Shapiro <23049337+crypterio-app@users.noreply.github.com>
2025-08-30 21:55:14 +0300

    update

---
+++ variable.tf
@@ -0,0 +1,32 @@
+
+
+variable "kind_cluster_name" {
+  description = "The name of the Kind cluster"
+  type        = string
+}
+
+variable "kind_cluster_config_path" {
+  description = "Path to save the Kind kubeconfig file"
+  type        = string
+}
+
+variable "charts_path" {
+  default     = "./charts"
+  description = "The charts full path"
+}
+
+variable "k8s_version" {
+  description = "Kubernetes version to use for Kind nodes"
+  type        = string
+}
+
+variable "worker_count" {
+  description = "Number of worker nodes"
+  type        = number
+}
+
+variable "additional_control_planes_count" {
+  description = "Number of additional control planes for HA"
+  type        = number
+  default     = 2
+}
